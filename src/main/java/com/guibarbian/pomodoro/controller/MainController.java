bb50a6589e94f6abc817d894261953ddbb9b51d9
guibarbian <guiabarbian2@gmail.com>
2026-01-21 02:51:15 -0300

    feat: pagina de configurações para o pomodoro criada, botão Iniciar configurado para enviar para ela

--- src/main/java/com/guibarbian/pomodoro/controller/MainController.java
+++ src/main/java/com/guibarbian/pomodoro/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.guibarbian.pomodoro.controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -9,6 +15,15 @@ public class MainController {
     
     @FXML
     private Stage stage;
+    private Scene scene;
+
+    public void handleIniciar(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/com/guibarbian/pomodoro/fxml/PomodoroSettings.fxml"));
+        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void handleSair(ActionEvent e){
         System.out.println("Saiu");
